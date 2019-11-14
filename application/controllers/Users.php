e81e8fa42da1614a4f863476f61b0f2f3d305038
Eddy Leon <32406542+eleon479@users.noreply.github.com>
2019-11-14 11:02:58 -0500

    implemented server side input validation

--- application/controllers/Users.php
+++ application/controllers/Users.php
@@ -2,41 +2,66 @@
 
 class Users extends CI_Controller {
 
-  public function __construct()
-  {
+  public function __construct() {
     parent::__construct();
     $this->load->model('users_model');
   }
 
-  public function index() { $this->load->view('users/create'); }
-  public function create() { $this->load->view('users/create'); }
+  public function index() {
+    $this->load->view('users/create');
+  }
+  public function create() {
+    $this->load->view('users/create');
+  }
 
-  private function validateCreateRequest()
-  {
+  private function validateCreateRequest() {
+    $rules = [
+      'date' => function($date) { 
+        return (bool) date_create($date);
+      },
+      'email' => function($email) { 
+        return (bool) filter_var($email, FILTER_VALIDATE_EMAIL);
+      },
+      'required' => function($val) {
+        if ($val == '') return false;
+        else return true;
+      }
+    ];
+
+    $fieldRuleSet = [
+      'fullname' => ['required'],
+      'dob' => ['required', 'date'],
+      'email' => ['required', 'email'],
+      'color' => ['required']
+    ];
+
+    foreach ($fieldRuleSet as $field => $fieldRules) {
+      foreach ($fieldRules as $ruleType) {
+        if (!$rules[$ruleType]($this->input->post($field))) {
+          return FALSE;
+        }
+      }
+    }
+
     return TRUE;
   }
 
-  public function createNewUser()
-  {
+  public function createNewUser() {
     // default: returned if validation fails
     $requestResponse = [
       'userCreated' => FALSE,
       'errorMessage' => 'Invalid or missing fields.'
     ];
-    
+
     // begin validation
-    if ($this->validateCreateRequest())
-    {
-      try
-      {
+    if ($this->validateCreateRequest()) {
+      try {
         $this->users_model->set_users();
         $requestResponse = [
           'userCreated' => TRUE,
           'errorMessage' => NULL
         ];
-      }
-      catch (Exception $e)
-      {
+      } catch (Exception $e) {
         // something went wrong with model / data layer
         $requestResponse = [
           'userCreated' => FALSE,
@@ -47,7 +72,6 @@ class Users extends CI_Controller {
 
     echo json_encode($requestResponse);
   }
-
 }
 
 ?>
